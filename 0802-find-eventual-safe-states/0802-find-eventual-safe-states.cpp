955642808c67f55116279958f0d4d6111be38ac6
nishantdubey-tech <nishantdubey2021@gmail.com>
2026-09-15 11:40:51 +0530

    Time: 8 ms (76.36%), Space: 51.8 MB (88.34%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
+       
+        if (state[node] != 0) {
+            return state[node] == 2;
+        }
+        state[node] = 1; 
+        for (int neighbor : graph[node]) {
+            if (!dfs(neighbor, graph, state)) {
+                return false; 
+            }
+        }
+        state[node] = 2; 
+        return true;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> state(n, 0);
+        vector<int> safeNodes;
+        
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, graph, state)) {
+                safeNodes.push_back(i);
+            }
+        }
+        
+        return safeNodes;
+    }
+};
